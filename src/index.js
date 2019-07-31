40ef12fe8c19517217e8bb9d7cd94d42eb2136e1
shantipdewi <shantipdewi@gmail.com>
2019-07-31 10:47:17 +0700

    editbray

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Selamat datang di HEROKU, di PORT ${port}</h1>`)
+})
 app.listen(port, () => {
     console.log('Berhasil Running di ' + port);
     
